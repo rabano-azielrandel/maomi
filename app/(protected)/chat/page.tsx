1a21e76c64155f9732b285fd92dbd5899b62f7d5
Aziel Randel Rabano <imazielr@gmail.com>
2026-03-12 00:04:10 +0800

    create initial chat page

---
+++ app/(protected)/chat/page.tsx
@@ -0,0 +1,16 @@
+const page = () => {
+  return (
+    <div className="flex flex-col">
+      <p>this is chat</p>
+      <p>this is chat</p>
+      <p>this is chat</p>
+      <p>this is chat</p>
+      <p>this is chat</p>
+      <p>this is chat</p>
+      <p>this is chat</p>
+      <p>this is chat</p>
+    </div>
+  );
+};
+
+export default page;
