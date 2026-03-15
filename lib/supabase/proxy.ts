075de24134084f2c61bad1dfbdbca1b1b0d90f29
Aziel Randel Rabano <imazielr@gmail.com>
2026-03-15 20:22:01 +0800

    update proxy to handle forgot pass and update pass, its not fully working but its now accessible

--- lib/supabase/proxy.ts
+++ lib/supabase/proxy.ts
@@ -52,7 +52,9 @@ export async function updateSession(request: NextRequest) {
     !user &&
     !request.nextUrl.pathname.startsWith("/login") &&
     !request.nextUrl.pathname.startsWith("/auth") &&
-    !request.nextUrl.pathname.startsWith("/sign-up") 
+    !request.nextUrl.pathname.startsWith("/sign-up") && 
+    !request.nextUrl.pathname.startsWith("/forgot-password") &&
+    !request.nextUrl.pathname.startsWith("/update-password") // for fix, auth session is missing
   ) {
     // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone();
