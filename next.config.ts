391999b381ba8ad85d2f37ce8873131a49144062
Aziel Randel Rabano <imazielr@gmail.com>
2026-03-27 09:00:27 +0800

    add the domain to next.config.js to use supabase bucket images

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   cacheComponents: true,
+  images: {
+    domains: ["tostzlvgedhrlwxywrdx.supabase.co"],
+  }
 };
 
 export default nextConfig;
