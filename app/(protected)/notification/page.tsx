57171f2aab92b6d9eec4d469f7d80bab67059473
Aziel Randel Rabano <imazielr@gmail.com>
2026-03-31 13:48:12 +0800

    create initial notification

---
+++ app/(protected)/notification/page.tsx
@@ -0,0 +1,10 @@
+export default function page() {
+  return (
+    <div>
+      <p>this is notification</p>
+      <p>this is notification</p>
+      <p>this is notification</p>
+      <p>this is notification</p>
+    </div>
+  );
+}
