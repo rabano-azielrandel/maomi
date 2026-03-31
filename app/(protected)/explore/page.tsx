5828db74651a7a57fc8e00d2595b379922f55a0d
Aziel Randel Rabano <imazielr@gmail.com>
2026-03-31 13:46:05 +0800

    add initial explore

---
+++ app/(protected)/explore/page.tsx
@@ -0,0 +1,11 @@
+export default function page() {
+  return (
+    <div>
+      <p>This is for explore</p>
+      <p>This is for explore</p>
+      <p>This is for explore</p>
+      <p>This is for explore</p>
+      <p>This is for explore</p>
+    </div>
+  );
+}
